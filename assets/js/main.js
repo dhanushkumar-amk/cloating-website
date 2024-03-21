4eda39145e66095d75fb383690954c4c4c5c5711
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-03-21 09:05:14 +0530

    uploaded 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,18 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
-
+/*=============== CHANGE BACKGROUND HEADER ===============*/const scrollHeader = () => {
+  const header = document.getElementById('header');
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+};
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== SWIPER PRODUCTS ===============*/
 
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
